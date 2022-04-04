1e33656ed2a409eb052b99a77c171bb11587e14f
Gustavo Ciotto Pinton <ciotto@amazon.com>
2022-04-04 20:06:16 -0300

    OPENCL: improve test scripts.

--- opencl/test/test_de.sh
+++ opencl/test/test_de.sh
@@ -19,8 +19,10 @@ POPULATION_SIZE="32"
 function de_common_mid_point {
     DATA_NAME=${1}
     AZIMUTH=${2}
+    TEST_INDEX=${3}
+    THREAD_COUNT=${4}
 
-    TEST_OUTPUT_DIR="${TEST_DIR}/cmp/de/${FRAMEWORK}/${DATA_NAME}"
+    TEST_OUTPUT_DIR="${TEST_DIR}/cmp/de/${FRAMEWORK}/${DATA_NAME}_${TEST_INDEX}"
 
     mkdir -p ${TEST_OUTPUT_DIR}
 
@@ -33,14 +35,17 @@ function de_common_mid_point {
         --generations ${GENERATIONS} --population-size ${POPULATION_SIZE} \
         --traveltime cmp \
         --kernel-path ${KERNEL_PATH} \
+        --thread-count ${THREAD_COUNT} \
         --verbose 1 | tee ${TEST_OUTPUT_DIR}/output.log)
 }
 
 function de_zero_offset_reflection_surface {
     DATA_NAME=${1}
     AZIMUTH=${2}
+    TEST_INDEX=${3}
+    THREAD_COUNT=${4}
 
-    TEST_OUTPUT_DIR="${TEST_DIR}/zocrs/de/${FRAMEWORK}/${DATA_NAME}"
+    TEST_OUTPUT_DIR="${TEST_DIR}/zocrs/de/${FRAMEWORK}/${DATA_NAME}_${TEST_INDEX}"
 
     mkdir -p ${TEST_OUTPUT_DIR}
 
@@ -53,14 +58,17 @@ function de_zero_offset_reflection_surface {
         --generations ${GENERATIONS} --population-size ${POPULATION_SIZE} \
         --traveltime zocrs --ain 60 --v0 2000 --bpctg 0.1 \
         --kernel-path ${KERNEL_PATH} \
+        --thread-count ${THREAD_COUNT} \
         --verbose 1 | tee ${TEST_OUTPUT_DIR}/output.log)
 }
 
 function de_offset_continuation_trajectory {
     DATA_NAME=${1}
     AZIMUTH=${2}
+    TEST_INDEX=${3}
+    THREAD_COUNT=${4}
 
-    TEST_OUTPUT_DIR="${TEST_DIR}/oct/de/${FRAMEWORK}/${DATA_NAME}"
+    TEST_OUTPUT_DIR="${TEST_DIR}/oct/de/${FRAMEWORK}/${DATA_NAME}_${TEST_INDEX}"
 
     mkdir -p ${TEST_OUTPUT_DIR}
 
@@ -73,20 +81,44 @@ function de_offset_continuation_trajectory {
         --generations ${GENERATIONS} --population-size ${POPULATION_SIZE} \
         --traveltime oct \
         --kernel-path ${KERNEL_PATH} \
+        --thread-count ${THREAD_COUNT} \
         --verbose 1 | tee ${TEST_OUTPUT_DIR}/output.log)
 }
 
-#### Common Mid Point
+## Example 1: Run Common Mid Point 10 times
+# THREAD_COUNT=64
+# for i in `seq 1 10`;
+# do
+#    echo "Executing ${i}th iteration - CMP"
+#    de_common_mid_point "fold2000" "90" ${i} ${THREAD_COUNT}
+#    de_common_mid_point "simple-synthetic" "0" ${i} ${THREAD_COUNT}
+# done
 
-de_common_mid_point "fold2000" "90"
-de_common_mid_point "simple-synthetic" "0"
+## Example 2: Run Zero Offset Common Reflection Point 10 times
+# THREAD_COUNT=64
+# for i in `seq 1 10`;
+# do
+#     echo "Executing ${i}th iteration - ZOCRS"
+#     de_zero_offset_reflection_surface "fold2000" "90" ${i} ${THREAD_COUNT}
+#     de_zero_offset_reflection_surface "simple-synthetic" "0" ${i} ${THREAD_COUNT}
+# done
 
-#### Zero Offset Common Reflection Point
+## Example 3: Run Offset Continuation Trajectory 10 times
+# THREAD_COUNT=64
+# for i in `seq 1 10`;
+# do
+#     echo "Executing ${i}th iteration - OCT"
+#     de_offset_continuation_trajectory "fold2000" "90" ${i} ${THREAD_COUNT}
+#     de_offset_continuation_trajectory "simple-synthetic" "0" ${i} ${THREAD_COUNT}
+# done
 
-de_zero_offset_reflection_surface "fold2000" "90"
-de_zero_offset_reflection_surface "simple-synthetic" "0"
-
-#### Offset Continuation Trajectory
-
-de_offset_continuation_trajectory "fold2000" "90"
-de_offset_continuation_trajectory "simple-synthetic" "0"
+## Example 4: Assess different threads per block
+# THREAD_COUNTS=(32 64 128 256 512 1024)
+# for THREAD_COUNT in ${THREAD_COUNTS[@]}; do
+#     for i in `seq 1 3`;
+#     do
+#         echo "Executing ${i}th iteration - ZOCRS"
+#         de_zero_offset_reflection_surface "fold2000" "90" ${i} ${THREAD_COUNT}
+#         de_zero_offset_reflection_surface "simple-synthetic" "0" ${i} ${THREAD_COUNT}
+#     done
+# done
--- opencl/test/test_greedy.sh
+++ opencl/test/test_greedy.sh
@@ -16,8 +16,10 @@ KERNEL_PATH="${OPENCL_ROOT}/src/semblance/kernel"
 function greedy_common_mid_point {
     DATA_NAME=${1}
     AZIMUTH=${2}
+    TEST_INDEX=${3}
+    THREAD_COUNT=${4}
 
-    TEST_OUTPUT_DIR="${TEST_DIR}/cmp/greedy/${FRAMEWORK}/${DATA_NAME}"
+    TEST_OUTPUT_DIR="${TEST_DIR}/cmp/greedy/${FRAMEWORK}/${DATA_NAME}_${TEST_INDEX}"
 
     mkdir -p ${TEST_OUTPUT_DIR}
 
@@ -30,14 +32,17 @@ function greedy_common_mid_point {
         --granularity 1024 \
         --traveltime cmp \
         --kernel-path ${KERNEL_PATH} \
+        --thread-count ${THREAD_COUNT} \
         --verbose 1 | tee ${TEST_OUTPUT_DIR}/output.log)
 }
 
 function greedy_zero_offset_reflection_surface {
     DATA_NAME=${1}
     AZIMUTH=${2}
+    TEST_INDEX=${3}
+    THREAD_COUNT=${4}
 
-    TEST_OUTPUT_DIR="${TEST_DIR}/zocrs/greedy/${FRAMEWORK}/${DATA_NAME}"
+    TEST_OUTPUT_DIR="${TEST_DIR}/zocrs/greedy/${FRAMEWORK}/${DATA_NAME}_${TEST_INDEX}"
 
     mkdir -p ${TEST_OUTPUT_DIR}
 
@@ -50,14 +55,17 @@ function greedy_zero_offset_reflection_surface {
         --granularity 100 10 10 \
         --traveltime zocrs --ain 60 --v0 2000 --bpctg 0.1 \
         --kernel-path ${KERNEL_PATH} \
+        --thread-count ${THREAD_COUNT} \
         --verbose 1 | tee ${TEST_OUTPUT_DIR}/output.log)
 }
 
 function greedy_offset_continuation_trajectory {
     DATA_NAME=${1}
     AZIMUTH=${2}
+    TEST_INDEX=${3}
+    THREAD_COUNT=${4}
 
-    TEST_OUTPUT_DIR="${TEST_DIR}/oct/greedy/${FRAMEWORK}/${DATA_NAME}"
+    TEST_OUTPUT_DIR="${TEST_DIR}/oct/greedy/${FRAMEWORK}/${DATA_NAME}_${TEST_INDEX}"
 
     mkdir -p ${TEST_OUTPUT_DIR}
 
@@ -70,20 +78,44 @@ function greedy_offset_continuation_trajectory {
         --granularity 100 100 \
         --traveltime oct \
         --kernel-path ${KERNEL_PATH} \
+        --thread-count ${THREAD_COUNT} \
         --verbose 1 | tee ${TEST_OUTPUT_DIR}/output.log)
 }
 
-#### Common Mid Point
+## Example 1: Run Common Mid Point 10 times
+# THREAD_COUNT=64
+# for i in `seq 1 10`;
+# do
+#    echo "Executing ${i}th iteration - CMP"
+#    greedy_common_mid_point "fold2000" "90" ${i} ${THREAD_COUNT}
+#    greedy_common_mid_point "simple-synthetic" "0" ${i} ${THREAD_COUNT}
+# done
 
-greedy_common_mid_point "fold2000" "90"
-greedy_common_mid_point "simple-synthetic" "0"
+## Example 2: Run Zero Offset Common Reflection Point 10 times
+# THREAD_COUNT=64
+# for i in `seq 1 10`;
+# do
+#     echo "Executing ${i}th iteration - ZOCRS"
+#     greedy_zero_offset_reflection_surface "fold2000" "90" ${i} ${THREAD_COUNT}
+#     greedy_zero_offset_reflection_surface "simple-synthetic" "0" ${i} ${THREAD_COUNT}
+# done
 
-#### Zero Offset Common Reflection Point
+## Example 3: Run Offset Continuation Trajectory 10 times
+# THREAD_COUNT=64
+# for i in `seq 1 10`;
+# do
+#     echo "Executing ${i}th iteration - OCT"
+#     greedy_offset_continuation_trajectory "fold2000" "90" ${i} ${THREAD_COUNT}
+#     greedy_offset_continuation_trajectory "simple-synthetic" "0" ${i} ${THREAD_COUNT}
+# done
 
-greedy_zero_offset_reflection_surface "fold2000" "90"
-greedy_zero_offset_reflection_surface "simple-synthetic" "0"
-
-#### Offset Continuation Trajectory
-
-greedy_offset_continuation_trajectory "fold2000" "90"
-greedy_offset_continuation_trajectory "simple-synthetic" "0"
+## Example 4: Assess different threads per block
+# THREAD_COUNTS=(32 64 128 256 512 1024)
+# for THREAD_COUNT in ${THREAD_COUNTS[@]}; do
+#     for i in `seq 1 3`;
+#     do
+#         echo "Executing ${i}th iteration - CMP"
+#         greedy_common_mid_point "fold2000" "90" ${i} ${THREAD_COUNT}
+#         greedy_common_mid_point "simple-synthetic" "0" ${i} ${THREAD_COUNT}
+#     done
+# done
